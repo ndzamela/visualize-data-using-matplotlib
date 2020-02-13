3f7f50ae68f0aa35b91a9cbe8a6fa5dcca541c5b
ndzamela <ndzamela@users.noreply.github.com>
2020-02-13 08:40:45 +0200

    Code file created.

---
+++ code.py
@@ -0,0 +1,31 @@
+# --------------
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+electors_2009 = pd.read_csv(path)
+candidate_2009 = pd.read_csv(path1)
+
+# Plot a bar chart to compare the number of male and female candidates in the election
+
+# Plot a histogram of the age of all the candidates as well as of the winner amongst them. Compare them and note an observation
+
+# Plot a bar graph to get the vote shares of different parties
+
+# Plot a barplot to compare the mean poll percentage of all the states
+
+# Plot a bar plot to compare the seats won by different parties in Uttar Pradesh
+
+# Plot a stacked bar chart to compare the number of seats won by different `Alliances` in Gujarat,Madhya Pradesh and Maharashtra. 
+
+# Plot a grouped bar chart to compare the number of winner candidates on the basis of their caste in the states of Andhra Pradesh, Kerala, Tamil Nadu and Karnataka
+
+# Plot a horizontal bar graph of the Parliamentary constituency with total voters less than 100000
+
+# Plot a pie chart with the top 10 parties with majority seats in the elections
+
+# Plot a pie diagram for top 9 states with most number of seats
+
+
